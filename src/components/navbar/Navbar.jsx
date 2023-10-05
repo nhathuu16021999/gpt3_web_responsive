1aace5cb7c26ffe762c95aaf10f39ad081c5b0af
nhathuu16021999 <49590630+nhathuu16021999@users.noreply.github.com>
2023-10-05 14:53:53 +0700

    Create layout component what-gpt3

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Menu = () => (
       <a href='#home'>Home</a>
     </p>
     <p>
-      <a href='#wpt3'>What is GPT3?</a>
+      <a href='#w-gpt3'>What is GPT3?</a>
     </p>
     <p>
       <a href='#possibility'>Open AI</a>
